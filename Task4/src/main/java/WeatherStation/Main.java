e9911bb88dbae5ed12d1705677a314a4a9587b95
Daniel <100704382+Ristler@users.noreply.github.com>
2025-08-30 00:40:49 +0300

    Enhance Main class: add multiple Temperature observers and implement sleep for synchronization

--- Task4/src/main/java/WeatherStation/Main.java
+++ Task4/src/main/java/WeatherStation/Main.java
@@ -3,12 +3,16 @@ import WeatherStation.Observable.Concrete.ConcreteWeatherStation;
 import WeatherStation.Observer.Concrete.Temperature;
 
 public class Main {
-    public static void main(String[]args) {
+    public static void main(String[]args) throws InterruptedException {
         ConcreteWeatherStation station = new ConcreteWeatherStation();
         Temperature temp1 = new Temperature(station);
+        Temperature temp2 = new Temperature(station);
+        Temperature temp3 = new Temperature(station);
         Thread t1 = new Thread(station);
         t1.start();
+        Thread.sleep(2000);
 
-        //station.Detach(temp1);
+        station.Detach(temp1);
+
     }
 }
